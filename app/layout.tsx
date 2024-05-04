e77be7173f7eb2b74a23223d7fdfcd093c0edf7d
Duman <201605@astanait.edu.kz>
2024-05-04 19:12:43 +0500

    Update metadata and image URLs in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import "./globals.css";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -7,28 +6,28 @@ import Navbar from "./components/ui/navbar";
 import Footer from "./components/ui/footer";
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
-export const metadata: Metadata = {
-  openGraph: {
-    title: "Фестиваль школьного спорта",
-    description: "Фестиваль школьного спорта Самое масштабное событием в мире школьного спорта, на котором подведут итоги спортивного сезона 2023/2024 учебного года",
-    url: "https://school.moscow.sport/",
-    type: "website",
-    images: [
-      {
-        url: "/images/preview.png",
-        alt: "Фестиваль школьного спорта",
-      },
-    ],
-  },
-title: "Фестиваль школьного спорта",
-  description: "«1 июня большой спортивный праздник в Лужниках»",
-    icons: [
-      {
-        "rel": "icon",
-        "url": "/images/preview.png"
-      }
-    ]
-};
+// export const metadata: Metadata = {
+//   openGraph: {
+//     title: "Фестиваль школьного спорта",
+//     description: "Фестиваль школьного спорта Самое масштабное событием в мире школьного спорта, на котором подведут итоги спортивного сезона 2023/2024 учебного года",
+//     url: "https://school.moscow.sport/",
+//     type: "website",
+//     images: [
+//       {
+//         url: "/images/preview.png",
+//         alt: "Фестиваль школьного спорта",
+//       },
+//     ],
+//   },
+//   title: "Фестиваль школьного спорта",
+//   description: "«1 июня большой спортивный праздник в Лужниках»",
+//   icons: [
+//     {
+//       "rel": "icon",
+//       "url": "/images/preview.png"
+//     }
+//   ]
+// };
 
 export default function RootLayout({
   children,
@@ -37,6 +36,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="«Фестиваль школьного спорта»" />
+        <meta
+          property="description"
+          content="«1 июня большой спортивный праздник в Лужниках»"
+        />
+        <meta property="og:description" content="«Фестиваль школьного спорта» Самое масштабное событием в мире школьного спорта, на котором подведут итоги спортивного сезона 2023/2024 учебного года" />
+        <meta property="og:url" content="https://school.moscow.sport/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/images/preview.png" />
+        <title>Фестиваль школьного спорта</title>
+      </head>
       <body className="flex flex-col justify-between min-h-screen">
         <Navbar />
         <main>
